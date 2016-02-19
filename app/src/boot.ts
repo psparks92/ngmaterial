69ace6d772cd4bf71088b6a2b3cf1ce5df83fe4a
Peter Sparks HCO <psparks1992@gmail.com>
2016-02-19 08:31:20 -0600

    getting angular and ts working

--- app/src/boot.ts
+++ app/src/boot.ts
@@ -1,7 +1,4 @@
 /// <reference path="_all.ts" />
-let a = [1,2,3];
-let b = [...a,4,5,6];
-
-for (var i of b) {
-    i++;
+module ContactManagerApp {
+    angular.module('contactManagerApp', ['ngMaterial']);
 }
